13d097a01f0c4b70a6c08dde78aca357641754e0
Wjian1204 <wenjian1204@gmail.com>
2024-12-04 00:44:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 setInterval(() => {
-    fetch('http://blynk-cloud.com/<YourAuthToken>/get/V1')
+    fetch('http://blynk-cloud.com/BWETyo1RTml1Grzi1GAGfDSZ0xhtaHb5/get/V0')
         .then(response => response.json())
         .then(data => {
-            document.getElementById('sensorValue').innerText = data[0];  // Display the sensor value
+            document.getElementById('waterLevel1').innerText = data[0];  // Display the water level 1
         })
         .catch(error => console.log("Error fetching data: " + error));
 }, 1000);  // Update every 1 second
